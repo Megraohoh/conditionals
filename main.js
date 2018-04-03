760353a82a178502cb8bf6d165cc8c4f3ff48447
Meghan Debity <mdebity@gmail.com>
2018-04-03 13:45:34 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//Exercise: Using Javascript, create a variable to hold your last name. 
+//Using the if-else statement, do the following: 
+//If your last name has 7 or more letters, console log "I can't pronounce this last name". 
+//Else, console log your last name.
+
+//Challenge: Create a new repository on Github and push the code you wrote up to Github.
+function lastNameResponder();
+const lastName ="Debity";
+    if (lastName.length >= 7) {
+    console.log("I can't pronounce that last name");
+
+    } else {
+    console.log("The last name is ",lastName);  
+    }
+}
+lastNameResponder();
